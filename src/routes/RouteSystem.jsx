9249ba9075f46fbbecacc11cc73c24aea00c30f9
SHervas03 <sergiohervas9rgmail.com>
2024-11-23 14:56:51 +0100

    FAQ

--- src/routes/RouteSystem.jsx
+++ src/routes/RouteSystem.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Index from '../pages/Index.jsx'
 import MainLayout from '../layouts/MainLayout.jsx'
 import Recipes from '../pages/Recipes.jsx';
+import FAQ from '../pages/FAQ.jsx';
 
 function RouteSystem() {
 
@@ -11,6 +12,7 @@ function RouteSystem() {
                 <Route path="" element={<MainLayout />}>
                     <Route index element={<Index />} />
                     <Route path="Recetas" element={<Recipes />} />
+                    <Route path="FAQ" element={<FAQ />} />
                 </Route>
             </Routes>
         </BrowserRouter>
