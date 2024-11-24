982f667354c4f013468e57bc90319a3d98b737b2
SHervas03 <sergiohervas9rgmail.com>
2024-11-24 20:34:40 +0100

    FAQ y recetas

---
+++ src/data/FAQ.js
@@ -0,0 +1,20 @@
+const FAQ = [
+    {
+        "id": 1,
+        "question": "What is Flowbite?",
+        "answer": "Flowbite is an open-source library of interactive components built on top of Tailwind CSS including buttons, dropdowns, modals, navbars, and more."
+    },
+    {
+        "id": 2,
+        "question": "Is there a Figma file available?",
+        "answer": "Flowbite is first conceptualized and designed using the Figma software so everything you see in the library has a design equivalent in our Figma file."
+    },
+    {
+        "id": 3,
+        "question": "What are the differences between Flowbite and Tailwind UI?",
+        "answer": "The main difference is that the core components from Flowbite are open source under the MIT license, whereas Tailwind UI is a paid product."
+    }
+]
+    ;
+
+export default FAQ;
---
+++ src/data/Menus.js
@@ -0,0 +1,60 @@
+const Menus = [
+    {
+        "id": 1,
+        "title": "Noteworthy technology",
+        "description": "Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.",
+        "image": "https://arquivet.com/img/cms/De%20que%20esta%20hecho%20el%20pienso%20para%20perros.jpg",
+        "link": "#"
+    },
+    {
+        "id": 2,
+        "title": "AI advancements in 2022",
+        "description": "Discover the latest advancements in artificial intelligence and how they're shaping the world.",
+        "image": "https://arquivet.com/img/cms/De%20que%20esta%20hecho%20el%20pienso%20para%20perros.jpg",
+        "link": "#"
+    },
+    {
+        "id": 3,
+        "title": "Sustainable Tech Solutions",
+        "description": "Explore how technology is driving sustainability and eco-friendly solutions.",
+        "image": "https://arquivet.com/img/cms/De%20que%20esta%20hecho%20el%20pienso%20para%20perros.jpg",
+        "link": "#"
+    },
+    {
+        "id": 4,
+        "title": "The future of blockchain",
+        "description": "Blockchain technology is evolving rapidly. Learn about its future applications.",
+        "image": "https://arquivet.com/img/cms/De%20que%20esta%20hecho%20el%20pienso%20para%20perros.jpg",
+        "link": "#"
+    },
+    {
+        "id": 5,
+        "title": "Noteworthy technology",
+        "description": "Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.",
+        "image": "https://arquivet.com/img/cms/De%20que%20esta%20hecho%20el%20pienso%20para%20perros.jpg",
+        "link": "#"
+    },
+    {
+        "id": 6,
+        "title": "AI advancements in 2022",
+        "description": "Discover the latest advancements in artificial intelligence and how they're shaping the world.",
+        "image": "https://arquivet.com/img/cms/De%20que%20esta%20hecho%20el%20pienso%20para%20perros.jpg",
+        "link": "#"
+    },
+    {
+        "id": 7,
+        "title": "Sustainable Tech Solutions",
+        "description": "Explore how technology is driving sustainability and eco-friendly solutions.",
+        "image": "https://arquivet.com/img/cms/De%20que%20esta%20hecho%20el%20pienso%20para%20perros.jpg",
+        "link": "#"
+    },
+    {
+        "id": 8,
+        "title": "The future of blockchain",
+        "description": "Blockchain technology is evolving rapidly. Learn about its future applications.",
+        "image": "https://arquivet.com/img/cms/De%20que%20esta%20hecho%20el%20pienso%20para%20perros.jpg",
+        "link": "#"
+    },
+];
+
+export default Menus;
