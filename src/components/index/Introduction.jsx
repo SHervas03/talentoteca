0c753b2ae6c872647edf2b8f412495c66497f45c
Hervas_03 <106432670+SHervas03@users.noreply.github.com>
2024-12-02 10:30:30 +0100

    Update Introduction.jsx

--- src/components/index/Introduction.jsx
+++ src/components/index/Introduction.jsx
@@ -5,7 +5,7 @@ function Introduction() {
                 <div className="mx-auto max-w-5xl py-6 sm:py-16 lg:py-32">
                     <div className="text-center">
                         <h1 className="text-balance text-5xl font-semibold tracking-tight text-gray-900 sm:text-6xl">
-                            Innovando para Todos: Productos que Conectan a Tu Familia (¡y tu Mascota!)
+                            Innovando para Todos: Productos que Conectan a Tu Familia (¡y tu Perro!)
                         </h1>
                         <p className="mt-10 text-pretty text-lg font-medium text-gray-500 sm:text-xl/10">
                             En colaboración con Kellogg's, estamos desarrollando productos que no solo nutren, sino que también fortalecen el vínculo entre toda tu familia, incluyendo a tus amigos peludos.
